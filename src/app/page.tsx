dd3864aa516631fc11888d97c2ed4a05cb0fd937
drubin <daniel.rubin@xpecto.com>
2023-07-10 16:06:25 +0200

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const RootPage = () => {
-  const router = useRouter();
-
-  router.replace("/login");
+  // const router = useRouter();
+  //
+  // router.replace("/login");
   return <div className="w-full h-full flex items-center justify-center text-9xl font-bold">404</div>;
 };
 
