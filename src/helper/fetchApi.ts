526f3507993bddad45e8e140e3f69782b1aafb72
Daniel <contact@daniel-rubin.de>
2023-07-31 20:55:06 +0200

    refactor: made api calls async

--- src/helper/fetchApi.ts
+++ src/helper/fetchApi.ts
@@ -9,44 +9,42 @@ export interface TokenResponse extends CustomResponse {
   token?: string;
 }
 
-const fetchApi = (url: string, options?: RequestInit) => {
-  const result = fetch(`${process.env.API_URL}${url}`, {
+const fetchApi = async (url: string, options?: RequestInit) => {
+  return fetch(`${process.env.API_URL}${url}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
     },
   }).then((res) => res.json());
-
-  return result;
 };
 
-const register = (body: Object): Promise<TokenResponse> => {
+const register = async (body: Object): Promise<TokenResponse> => {
   return fetchApi("/auth/register/", { method: "POST", body: JSON.stringify(body) }).then(
     (res) => res as TokenResponse,
   );
 };
 
-const login = (body: Object): Promise<TokenResponse> => {
+const login = async (body: Object): Promise<TokenResponse> => {
   return fetchApi("/auth/login/", { method: "POST", body: JSON.stringify(body) }).then((res) => res as TokenResponse);
 };
 
-const getTasks = () => {
+const getTasks = async () => {
   return fetchApi("/tasks/", { method: "GET" }).then((res) => res as Task[]);
 };
 
-const getUser = (token: string) => {
+const getUser = async (token: string) => {
   return fetchApi("/contacts/user/", { method: "GET", headers: { Authorization: `Token ${token}` } });
 };
 
-const getContacts = (): Promise<User[]> => {
+const getContacts = async (): Promise<User[]> => {
   return fetchApi("/contacts/", { method: "GET" }).then((res) => res);
 };
 
-const createTask = (task: Task) => {
+const createTask = async (task: Task) => {
   return fetchApi("/tasks/", { method: "POST", body: JSON.stringify(task) });
 };
 
-const updateTask = (task: Task) => {
+const updateTask = async (task: Task) => {
   return fetchApi(`/tasks/${task.id}`, { method: "PATCH", body: JSON.stringify(task) });
 };
 
