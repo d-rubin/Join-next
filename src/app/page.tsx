ff69948e242154067318a8bb73748ea7e144411a
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-10-02 15:58:00 +0200

    fix: build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const RootPage = () => {
-  useRouter().replace("login");
+  // useRouter().replace("login");
   return <div className="w-full h-full flex items-center justify-center text-9xl font-bold">404</div>;
 };
 
