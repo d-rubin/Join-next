9bb8d1cfe2ec3fbced6073a53a39a162e25d9005
Daniel Rubin <daniel.rubin1997@gmail.com>
2023-10-07 10:15:04 +0200

    feat: added analaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="de" className="min-w-full min-h-screen">
       <body className={`${inter.className} min-w-full min-h-screen bg-defaultColor max-w-screen-lg mx-auto`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
