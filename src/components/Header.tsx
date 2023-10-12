13c5bbafcf436ac3411d7f2bec30c5052fdef6fe
Daniel <contact@daniel-rubin.de>
2023-10-12 19:34:32 +0200

    feat: finished Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,25 @@
-import React, { ReactNode } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import LogoDark from "../iconlib/logo-dark.svg";
 import Profile from "./Profile";
+import Icon from "./Icon";
+import Text from "./Text";
 
 const Header = () => {
   // Todo: Fetch user and
 
   return (
     <div className="w-full h-16 bg-white shadow-xl flex items-center px-4 justify-between">
-      <Link className="flex items-center md:hidden" href="/summary">
+      <Link className="flex items-center lg:hidden" href="/summary">
         <Image className="h-8 w-fit object-cover" src={LogoDark} alt="Logo" />
       </Link>
-      <Profile letters="D" />
+      <Text text="Kanban Project Management Tool" className="hidden lg:block" />
+      <div className="flex gap-4 items-center">
+        <Link href="help">
+          <Icon icon="help" className="hidden lg:block stroke-gray-300 fill-gray-300" iconSize="h-8 w-8" />
+        </Link>
+        <Profile letters="D" />
+      </div>
     </div>
   );
 };
