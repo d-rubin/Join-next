a12922e5f1d1a0d8bb3c59c0e9d36c6efb164132
Daniel <contact@daniel-rubin.de>
2023-11-11 15:37:32 +0100

    feat: added prio to BoardTask and status to edit Task

--- src/components/BoardTask.tsx
+++ src/components/BoardTask.tsx
@@ -28,6 +28,7 @@ const BoardTask = ({ task, contacts }: { task: Task; contacts: Contact[] }) => {
   const [prio, setPrio] = useState<PrioType | undefined>(task ? task.priority : undefined);
 
   const onSubmit = async (fieldValues: FieldValues) => {
+    console.log(fieldValues);
     await updateTask({ ...fieldValues, ...{ priority: prio || "low" } }).then((res) => {
       if (!res) {
         setDialogOpen(false);
@@ -36,10 +37,16 @@ const BoardTask = ({ task, contacts }: { task: Task; contacts: Contact[] }) => {
     });
   };
 
+  const getIconForPriority = (priority: PrioType) => {
+    if (priority === "low") return <Icon iconSize="h-4 w-4" icon="low" className="stroke-green fill-green" />;
+    if (priority === "medium") return <Icon iconSize="h-4 w-4" icon="medium" className="stroke-orange fill-orange" />;
+    return <Icon iconSize="h-4 w-4" icon="urgent" className="stroke-red fill-red" />;
+  };
+
   return (
     <Fragment key={task.id}>
       <div
-        className="p-4 min-w-40 w-52 bg-white rounded-3xl flex flex-col justify-between gap-2 lg:h-fit lg:w-full cursor-pointer"
+        className="p-4 min-w-40 w-52 bg-white rounded-3xl flex flex-col justify-start gap-2 lg:h-fit lg:w-full cursor-pointer"
         draggable
         onDragStart={() => updateDraggedTask(task)}
         onDragEnd={() => updateDraggedTask(null)}
@@ -53,141 +60,174 @@ const BoardTask = ({ task, contacts }: { task: Task; contacts: Contact[] }) => {
         </p>
         <p className="text-lg font-bold">{task.title}</p>
         <p className="text-gray-500">{task.description}</p>
+        <span className="w-full flex align-bottom justify-end">{getIconForPriority(task.priority)}</span>
       </div>
       <dialog className="fixed top-0 left-0 w-screen h-full bg-transparent" open={dialogOpen}>
         <div className="flex items-center justify-center w-full h-full bg-transparent">
-          <div className="z-10 w-fit h-fit bg-white rounded-3xl p-4 min-w-[20rem] shadow-2xl">
+          <div className="z-10 w-fit h-fit bg-white rounded-3xl p-4 min-w-[17rem] max-w-[25rem] shadow-2xl">
             {editTask ? (
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 relative">
-                <input type="number" {...register("id", { value: task.id })} hidden />
-                <Icon
-                  icon="arrowLeft"
-                  className="hover:stroke-underline hover:fill-underline absolute right-0 top-0"
-                  onClick={() => setEditTask(false)}
-                />
-                <input name="id" type="number" required defaultValue={task.id} className="hidden" />
-                <DefaultInput
-                  type="text"
-                  name="title"
-                  register={register}
-                  placeholder="Enter a title"
-                  block
-                  defaultValue={task ? task.title : undefined}
-                  isError={!!errors.title}
-                  errorText={errors.title?.message as string}
-                  label="Title"
-                />
-                <Textarea
-                  name="description"
-                  placeholder="Enter a description"
-                  block
-                  register={register}
-                  isError={!!errors.description}
-                  errorText={errors.description?.message as string}
-                  label="Description"
-                  className="h-20"
-                  defaultValue={task ? task.description : undefined}
-                />
-                <div className="flex flex-col gap-1">
-                  <p>Priority</p>
-                  <div className="flex flex-row gap-2">
-                    <Prio prio="urgent" active={prio === "high"} setPrio={setPrio} />
-                    <Prio prio="medium" active={prio === "medium"} setPrio={setPrio} />
-                    <Prio prio="low" active={prio === "low"} setPrio={setPrio} />
-                  </div>
-                </div>
-                <DefaultInput
-                  type="date"
-                  name="due_date"
-                  register={register}
-                  defaultValue={task ? task.due_date : undefined}
-                  isError={!!errors.due_date}
-                  errorText={errors.due_date?.message as string}
-                  block
-                  label="Due Date"
-                />
-                <div className="flex flex-col gap-1">
-                  <p>Category</p>
-                  <select
-                    required
-                    {...register("category", { value: task ? task.category : undefined })}
-                    className={clsx(
-                      `border-2 border-outline w-full rounded-lg px-3 focus:border-underline outline-none py-1.5`,
-                      {
-                        "border-red": !!errors.category,
-                      },
-                    )}
-                  >
-                    <option value="">Select task category</option>
-                    <option value="backoffice">Backoffice</option>
-                    <option value="design">Design</option>
-                    <option value="marketing">Marketing</option>
-                    <option value="sales">Sales</option>
-                    <option value="media">Media</option>
-                  </select>
-                  {errors.category && <p className="text-xs text-red">{errors.category.message as string}</p>}
-                </div>
-                <div className="flex flex-col gap-1">
-                  <p>Assignee</p>
-                  <select
-                    required
-                    {...register("assignee", { value: task ? task.assignee : undefined })}
-                    className={clsx(
-                      `border-2 border-outline w-full rounded-lg px-3 focus:border-underline outline-none py-1.5`,
-                      {
-                        "border-red": !!errors.assignee,
-                      },
-                    )}
-                  >
-                    <option>Select Assignee</option>
-                    {contacts.map((contact) => {
-                      return (
-                        <option value={contact.id} key={contact.email}>
-                          {contact.username}
-                        </option>
-                      );
-                    })}
-                  </select>
-                  {errors.assignee && <p className="text-xs text-red">{errors.assignee.message as string}</p>}
-                </div>
-                <span className="w-full flex justify-end">
-                  <BigButton text="Ok" icon="check" loading={isSubmitting} />
+              <div className="flex flex-col max-h-full">
+                <span className="w-full flex justify-end items-center">
+                  <Icon
+                    icon="arrowLeft"
+                    className="hover:stroke-underline hover:fill-underline"
+                    onClick={() => setEditTask(false)}
+                  />
                 </span>
-              </form>
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                  <input type="number" {...register("id", { value: task.id })} hidden />
+                  <input name="id" type="number" required defaultValue={task.id} className="hidden" />
+                  <DefaultInput
+                    type="text"
+                    name="title"
+                    register={register}
+                    placeholder="Enter a title"
+                    block
+                    defaultValue={task ? task.title : undefined}
+                    isError={!!errors.title}
+                    errorText={errors.title?.message as string}
+                    label="Title"
+                  />
+                  <Textarea
+                    name="description"
+                    placeholder="Enter a description"
+                    block
+                    register={register}
+                    isError={!!errors.description}
+                    errorText={errors.description?.message as string}
+                    label="Description"
+                    className="h-20"
+                    defaultValue={task ? task.description : undefined}
+                  />
+                  <div className="flex flex-col gap-1">
+                    <p>Priority</p>
+                    <div className="flex flex-row gap-2">
+                      <Prio prio="urgent" active={prio === "high"} setPrio={setPrio} />
+                      <Prio prio="medium" active={prio === "medium"} setPrio={setPrio} />
+                      <Prio prio="low" active={prio === "low"} setPrio={setPrio} />
+                    </div>
+                  </div>
+                  <DefaultInput
+                    type="date"
+                    name="due_date"
+                    register={register}
+                    defaultValue={task ? task.due_date : undefined}
+                    isError={!!errors.due_date}
+                    errorText={errors.due_date?.message as string}
+                    block
+                    label="Due Date"
+                  />
+                  <div className="flex flex-col gap-1">
+                    <p>Category</p>
+                    <select
+                      required
+                      {...register("category", { value: task ? task.category : undefined })}
+                      className={clsx(
+                        `border-2 border-outline w-full rounded-lg px-3 focus:border-underline outline-none py-1.5`,
+                        {
+                          "border-red": !!errors.category,
+                        },
+                      )}
+                    >
+                      <option value="">Select task category</option>
+                      <option value="backoffice">Backoffice</option>
+                      <option value="design">Design</option>
+                      <option value="marketing">Marketing</option>
+                      <option value="sales">Sales</option>
+                      <option value="media">Media</option>
+                    </select>
+                    {errors.category && <p className="text-xs text-red">{errors.category.message as string}</p>}
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <p>Assignee</p>
+                    <select
+                      required
+                      {...register("assignee")}
+                      className={clsx(
+                        `border-2 border-outline w-full rounded-lg px-3 focus:border-underline outline-none py-1.5`,
+                        {
+                          "border-red": !!errors.assignee,
+                        },
+                      )}
+                    >
+                      <option>Select Assignee</option>
+                      {contacts.map((contact) => {
+                        if (contact.id === task.assignee)
+                          return (
+                            <option value={contact.id} selected key={contact.email}>
+                              {contact.username}
+                            </option>
+                          );
+
+                        return (
+                          <option value={contact.id} key={contact.email}>
+                            {contact.username}
+                          </option>
+                        );
+                      })}
+                    </select>
+                    {errors.assignee && <p className="text-xs text-red">{errors.assignee.message as string}</p>}
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <p>Status:</p>
+                    <select
+                      required
+                      {...register("status", { value: task ? task.status : undefined })}
+                      className="border-2 border-outline w-full rounded-lg px-3 focus:border-underline outline-none py-1.5"
+                    >
+                      <option value="">Select task category</option>
+                      <option value="inProgress">in Progress</option>
+                      <option value="toDo">To do</option>
+                      <option value="awaitingFeedback">awaiting Feedback</option>
+                      <option value="done">Done</option>
+                    </select>
+                  </div>
+                  <span className="w-full flex justify-end">
+                    <BigButton text="Ok" icon="check" loading={isSubmitting} />
+                  </span>
+                </form>
+              </div>
             ) : (
               <div className="gap-4 flex flex-col relative">
-                <Icon
-                  icon="x"
-                  className="absolute right-0 top-0 hover:border-underline hover:stroke-underline hover:fill-underline"
-                  onClick={() => setDialogOpen(false)}
-                />
-                <p
-                  className="text-white px-4 py-1 w-fit rounded-lg"
-                  style={{ backgroundColor: getBackgroundForCategory(task.category) }}
-                >
-                  {generalHelper(task.category)}
-                </p>
-                <p className="text-3xl font-bold">{task.title}</p>
-                <p>{task.description}</p>
-                <span className="flex flex-row gap-2">
-                  <p className="w-1/3">Due Date:</p>
-                  <p className="w-2/2">
-                    {new Intl.DateTimeFormat("de-DE", { day: "2-digit", month: "2-digit", year: "numeric" }).format(
-                      new Date(task.due_date),
-                    )}
+                <span className="flex w-full justify-between items-center">
+                  <p
+                    className="text-white px-4 py-1 w-fit rounded-lg"
+                    style={{ backgroundColor: getBackgroundForCategory(task.category) }}
+                  >
+                    {generalHelper(task.category)}
                   </p>
+                  <Icon
+                    icon="x"
+                    className="hover:border-underline hover:stroke-underline hover:fill-underline"
+                    onClick={() => setDialogOpen(false)}
+                  />
                 </span>
-                <span className="flex flex-row gap-2">
-                  <p className="w-1/3">Priority:</p>
-                  <p className="w-2/2">
-                    {task.priority.charAt(0).toUpperCase()}
-                    {task.priority.slice(1)}{" "}
-                  </p>
-                </span>
-                <span className="flex flex-row gap-2">
-                  <p className="w-1/3">Assigned to:</p>
-                  <p className="w-2/2"> {getAssignee(task.assignee, contacts)}</p>
-                </span>
+                <div className="flex flex-col gap-2">
+                  <p className="text-3xl font-bold">{task.title}</p>
+                  <p className="text-black">{task.description}</p>
+                  <span className="flex">
+                    <p className="w-5/12 text-gray-600">Due Date:</p>
+                    <p className="w-7/12">
+                      {new Intl.DateTimeFormat("de-DE", { day: "2-digit", month: "2-digit", year: "numeric" }).format(
+                        new Date(task.due_date),
+                      )}
+                    </p>
+                  </span>
+                  <span className="flex">
+                    <p className="w-5/12 text-gray-600">Priority:</p>
+                    <span className="w-7/12 flex gap-2 items-center">
+                      <p>
+                        {task.priority.charAt(0).toUpperCase()}
+                        {task.priority.slice(1)}
+                      </p>
+                      <p>{getIconForPriority(task.priority)}</p>
+                    </span>
+                  </span>
+                  <span className="flex">
+                    <p className="w-5/12 text-gray-600">Assigned to:</p>
+                    <p className="w-7/12">{getAssignee(task.assignee, contacts)}</p>
+                  </span>
+                </div>
                 <span className="flex flex-row items-center gap-1 w-full justify-end">
                   <span
                     onClick={() => setEditTask(true)}
--- src/schemas.ts
+++ src/schemas.ts
@@ -34,6 +34,7 @@ const taskSchema = z.object({
   assignee: z.string().min(1, "Please select person to assign"),
   due_date: z.string().min(1, "Please add the date the task is scheduled"),
   category: z.string().min(3, "Select a category"),
+  status: z.string(),
 });
 
 export { loginSchema, signInSchema, taskSchema };
