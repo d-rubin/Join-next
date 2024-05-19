c28e2b7085030a7cec90764549248dfd7ea8dcd1
Daniel Rubin <daniel.rubin1997@gmail.com>
2024-05-19 11:33:11 +0200

    feat: add google-site-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const darkMode = cookies().get("darkMode")?.value === "true";
   return (
     <html lang="de-DE" className={cn("min-h-screen min-w-full", darkMode && "dark")}>
+      <head>
+        <meta name="google-site-verification" content="0q7Z2R5pfH1D0H3CjCkbBlQMZ0IYjor1C30vGqYuL7k" />
+      </head>
       <body className={`${inter.className} min-w-screen min-h-screen bg-defaultColor dark:bg-defaultColorDark`}>
         {children}
         <Analytics />
